1fb080cb5c505b578ed24e48d8892af90f667e68
Fabricyo Barreto <bass.barreto@gmail.com>
2025-11-09 21:39:23 -0300

    feat(consumer-analytics): drop null columns and add new total_price column

--- services/consumer-analyticis/app.py
+++ services/consumer-analyticis/app.py
@@ -5,4 +5,9 @@ response = requests.get('http://localhost:5000/data')
 data = response.json()
 
 df = pd.DataFrame(data)
+
+df.dropna()
+df["purchase_date"] = pd.to_datetime(df["purchase_date"])
+df["total_price"] = df["quantity"] * df["unit_price"]
+
 print(df.head())
